b23c63f8f8bae8e596e2cc0607ca6cbf5cb83f3b
turan.heydarli <turan.heydarli@CAGD4>
2023-04-09 12:47:30 +0400

    Add project files.

---
+++ ContactListApp/ContactList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactListApp
+{
+    class ContactList
+    {
+        Dictionary<string, string> _contactList = new Dictionary<string, string>();
+
+        public string GetNumber(string name)
+        {
+            return _contactList[name];
+        }
+
+        public void AddNumber(string name, string number)
+        {
+            _contactList.Add(number, name);
+        }
+
+        public void RemoveNumber(string name)
+        {
+            _contactList.Remove(name);
+        }
+    }
+}
---
+++ ContactListApp/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ContactListApp
+{
+    class Program
+    {
+        static ContactList _contactList = new ContactList();
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("1. Get Number\n 2.Add Number\n 3.Remove Number");
+
+            string operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "1":
+                    GetNumber();
+                    break;
+                case "2":
+                    AddNumber();
+                    break;
+
+            }
+
+        }
+
+        private static void AddNumber()
+        {
+            Console.WriteLine("Nomreni daxil edin: ");
+            string number = Console.ReadLine();
+            Console.WriteLine("Adi daxil edin: ");
+            string name = Console.ReadLine();
+
+            _contactList.AddNumber(name, number);
+        }
+
+        private static void GetNumber()
+        {
+            Console.WriteLine("Adi daxil edin: ");
+            string name = Console.ReadLine();
+
+            string number = _contactList.GetNumber(name);
+
+            Console.WriteLine($"Number of {name} is {number}");
+        }
+    }
+}
